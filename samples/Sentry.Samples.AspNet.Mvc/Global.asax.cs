0ff481857610eea4cd25cd3da4663407d6ae9094
Bruno Garcia <bruno@brunogarcia.com>
2019-02-21 11:11:43 +0100

    feat: Extract request data

--- samples/Sentry.Samples.AspNet.Mvc/Global.asax.cs
+++ samples/Sentry.Samples.AspNet.Mvc/Global.asax.cs
@@ -1,6 +1,8 @@
 using Sentry.EntityFramework;
+using Sentry.Extensibility;
 using System;
 using System.Configuration;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -26,10 +28,65 @@ namespace Sentry.Samples.AspNet.Mvc
             {
                 // We store the DSN inside Web.config; make sure to use your own DSN!
                 o.Dsn = new Dsn(ConfigurationManager.AppSettings["SentryDsn"]);
-         
                 // Add the EntityFramework integration
                 o.AddEntityFramework();
+                o.SendDefaultPii = true;
+                o.AddEventProcessor(new AspNetRequestProcessor(o));
             });
+        }
+
+        private class AspNetRequestProcessor : ISentryEventProcessor
+        {
+            private readonly SentryOptions _options;
+
+            public AspNetRequestProcessor(SentryOptions options)
+            {
+                _options = options ?? throw new ArgumentNullException(nameof(options));
+            }
+
+            public SentryEvent Process(SentryEvent @event)
+            {
+                HttpContext context = HttpContext.Current;
+                if (context == null)
+                {
+                    return @event;
+                }
+
+                @event.Request.Method = HttpContext.Current.Request.HttpMethod;
+                @event.Request.Url = context.Request.Path;
+
+                @event.Request.QueryString = context.Request.QueryString.ToString();
+                foreach (string key in context.Request.Headers.AllKeys)
+                {
+
+                    if (!_options.SendDefaultPii
+                        // Don't add headers which might contain PII
+                        && (key == "Cookie"
+                            || key == "Authorization"))
+                    {
+                        continue;
+                    }
+                    @event.Request.Headers[key] = context.Request.Headers[key];
+                }
+
+
+                if (_options?.SendDefaultPii == true)
+                {
+                    @event.Request.Env["REMOTE_ADDR"] = context.Request.UserHostAddress;
+                }
+
+                @event.User.Username = context.User.Identity?.Name;
+
+                @event.Request.Env["SERVER_NAME"] = Environment.MachineName;
+
+                string server = context.Response.Headers["Server"];
+                if (server != null)
+                {
+                    @event.Request.Env["SERVER_SOFTWARE"] = server;
+                }
+
+                return @event;
+            }
         }
 
         // Global error catcher
--- samples/Sentry.Samples.AspNet.Mvc/Models/IdentityModels.cs
+++ samples/Sentry.Samples.AspNet.Mvc/Models/IdentityModels.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Sentry.EntityFramework;
 
 namespace Sentry.Samples.AspNet.Mvc.Models
 {
